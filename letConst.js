ab3d96e40fa0603ced9581a8bc19e7a55110b953
smhasanjamil <smhasanjamil23@gmail.com>
2023-02-27 20:12:59 +0600

    Use of let

--- letConst.js
+++ letConst.js
@@ -1,7 +1,16 @@
 /*
-*==================== Use Of Var ====================
+*==================== Use Of var ====================
 */
+//In JavaScript, a var variable can be declared multiple times in the same scope without causing a syntax error.
 var Name = 'Hasan';
 Name = 'Jamil';
 var Name = 'Hasan Jamil';
-console.log(Name);
+//console.log(Name);
+
+/*
+*==================== Use Of let ====================
+*/
+//In JavaScript, a 'let' variable can be declared only once in a particular scope. 
+let num = 5;
+num = 7;
+console.log(num);
