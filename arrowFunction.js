67bc1f30b6580c00e8c64f16b43356afbbc322e7
smhasanjamil <smhasanjamil23@gmail.com>
2023-02-28 12:45:20 +0600

    Arrow Function

---
+++ arrowFunction.js
@@ -0,0 +1,32 @@
+//For single line
+/*
+const add = (num1, num2) => num1 + num2;
+console.log(add(3, 4));
+*/
+
+
+//For Multi Line
+const add = (num1, num2) => {
+    const sum = num1 + num2;
+    const multiply = sum * 3;
+    result = multiply;
+    return result;
+};
+//console.log(add(3, 4));
+
+
+
+//For one variable
+const singleAdd = num => {
+    const sum = num + 5;
+    const multiply = sum * 3;
+    result = multiply;
+    return result;
+};
+//console.log(singleAdd(5));
+
+
+
+//For No Variable
+const noVariable = () => console.log('Hello, Hasan!');
+noVariable();
