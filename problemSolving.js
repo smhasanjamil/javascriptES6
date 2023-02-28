c67a5a0207b1b2ebfaf557648fc7d3d23f9f1b1e
smhasanjamil <smhasanjamil23@gmail.com>
2023-02-28 17:07:26 +0600

    Problem: 4 || API Array Access

--- problemSolving.js
+++ problemSolving.js
@@ -59,4 +59,62 @@ persons.map(p => {
     info[id] = value;
 });
 //console.log(info);
-console.log(info.ID01);
+//console.log(info.ID01);
+
+
+/*
+*==================== API Array Access ====================
+*/
+const Students = [
+    {
+        student1: {
+            name: 'abul',
+            age: '17',
+            isMale: true,
+            education: {
+                class: 7,
+                subjects: ['bangla', 'english', 'math'],
+                school: 'durgapur high school',
+            }
+        },
+        student2: {
+            name: 'jarina',
+            age: '19',
+            isMale: false,
+            education: {
+                class: 6,
+                subjects: ['bangla', 'english', 'math'],
+                school: 'Rajshahi high school',
+            }
+        },
+        student3: {
+            name: 'habul',
+            age: '15',
+            isMale: true,
+            education: {
+                class: 9,
+                subjects: ['bangla', 'english', 'math'],
+                school: 'Dhaka high school',
+            }
+        }
+    }
+];
+
+
+
+Students.map(student => {
+    for (let i in student) {
+        //console.log(i);
+        const { name, age, isMale, education } = student[i];
+        const { subjects } = education;
+
+        const information = `
+        My Name is ${name}.
+        My age is ${age}.
+        My gender is ${isMale === true ? 'Male' : 'Female'},
+        My subject are: ${subjects.map(s => s)}`;
+
+        console.log(information);
+    }
+
+});
