9232b4711f68d56fafbc2c3db543eb7b5e810e01
smhasanjamil <smhasanjamil23@gmail.com>
2023-02-28 07:26:23 +0600

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,9 @@
+// Alternative of if else condition
+const isStudent = true;
+let x = 7;
+let paragraph = '';
+
+//(x === 7) ? console.log('Valid Output') : console.log('Invalid Output');
+paragraph = (isStudent === true) ? "He is a student" : "Poralekha valo lage nah";
+
+console.log(paragraph);
